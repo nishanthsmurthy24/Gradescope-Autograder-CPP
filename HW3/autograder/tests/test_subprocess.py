0cf3461d1ce7bb4dd9602d73b41c3afe0a188c11
Nishanth Somashekara Murthy <nishanthsmurthy24@gmail.com>
2023-12-30 03:01:10 -0600

    Added HW3 framework

---
+++ HW3/autograder/tests/test_subprocess.py
@@ -0,0 +1,82 @@
+import unittest
+from gradescope_utils.autograder_utils.decorators import weight
+import subprocess
+from difflib import Differ
+
+def compare_strings(string1, string2):
+    differ = Differ()
+    diff = list(differ.compare(string1.splitlines(), string2.splitlines()))
+    return '\n'.join(diff)
+
+class TestDiff(unittest.TestCase):
+    def setUp(self):
+        pass 
+
+    # Associated point value within GradeScope
+    @weight(5)
+    def test00(self):
+        #Title used by Gradescope 
+        """HW3A - Compilation Test"""
+
+        # Create a subprocess to run the students make file to ensure it compiles 
+        test = subprocess.Popen(["make", "a"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output = test.stderr.read().strip().decode('utf-8')
+        test.kill()
+
+        # Standard unit test case with an associated error message
+        if "error" in output:
+            print("COMPILATION FAILED!!")
+            self.assertTrue( output == "", msg=output)
+        elif "Stop" in output:
+            print("COMPILATION FAILED!!")
+            self.assertTrue( output == "", msg=output)
+        else:
+            print("COMPILATION SUCCESSFUL!!")
+        
+        test.terminate()
+
+    # Associated point value within GradeScope
+    @weight(5)
+    def test01(self):
+        #Title used by Gradescope 
+        """HW3B - Compilation Test"""
+
+        # Create a subprocess to run the students make file to ensure it compiles 
+        test = subprocess.Popen(["make", "b"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output = test.stderr.read().strip().decode('utf-8')
+        test.kill()
+
+        # Standard unit test case with an associated error message
+        if "error" in output:
+            print("COMPILATION FAILED!!")
+            self.assertTrue( output == "", msg=output)
+        elif "Stop" in output:
+            print("COMPILATION FAILED!!")
+            self.assertTrue( output == "", msg=output)
+        else:
+            print("COMPILATION SUCCESSFUL!!")
+        
+        test.terminate()
+
+    # Associated point value within GradeScope
+    @weight(5)
+    def test03(self):
+        #Title used by Gradescope 
+        """HW3C - Compilation Test"""
+
+        # Create a subprocess to run the students make file to ensure it compiles 
+        test = subprocess.Popen(["make", "c"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output = test.stderr.read().strip().decode('utf-8')
+        test.kill()
+
+        # Standard unit test case with an associated error message
+        if "error" in output:
+            print("COMPILATION FAILED!!")
+            self.assertTrue( output == "", msg=output)
+        elif "Stop" in output:
+            print("COMPILATION FAILED!!")
+            self.assertTrue( output == "", msg=output)
+        else:
+            print("COMPILATION SUCCESSFUL!!")
+        
+        test.terminate()
