1aea00889a3d419a0c286d605f8b92c5fd174446
Nishanth Somashekara Murthy <nishanthsmurthy24@gmail.com>
2024-02-07 05:12:18 -0600

    Added testcases for HW2B

--- HW3/autograder/tests/test_subprocess.py
+++ HW3/autograder/tests/test_subprocess.py
@@ -39,7 +39,7 @@ class TestDiff(unittest.TestCase):
         test.terminate()
     
     # Associated point value within GradeScope
-    @weight(2)
+    @weight(2.5)
     def test01(self):
         #Title used by Gradescope 
         """HW3A - Positive Integer"""
@@ -73,7 +73,7 @@ class TestDiff(unittest.TestCase):
         cat.terminate()
 
     # Associated point value within GradeScope
-    @weight(2)
+    @weight(2.5)
     def test02(self):
         #Title used by Gradescope 
         """HW3A - Negative Integer"""
@@ -109,7 +109,7 @@ class TestDiff(unittest.TestCase):
         cat.terminate()
 
     # Associated point value within GradeScope
-    @weight(4)
+    @weight(5)
     def test03(self):
         #Title used by Gradescope 
         """HW3A - Loop Condition"""
@@ -125,13 +125,13 @@ class TestDiff(unittest.TestCase):
             cat.kill()
             output = "Timeout expired!! Program did not exit."
 
-        reference = subprocess.check_output(["head", "-n", "18", "/autograder/source/reference/refA3.txt"]).decode('utf-8').strip()
+        reference = subprocess.check_output(["head", "-n", "25", "/autograder/source/reference/refA3.txt"]).decode('utf-8').strip()
 
         cat.kill()
 
-        print(f"Input Sequence: -12, 6\n")
+        print(f"Input Sequence: -12, 5, -8\n")
 
-        result = compare_strings(output, reference, 18)
+        result = compare_strings(output, reference, 25)
         print(result)
 
         # Standard unit test case with an associated error message
@@ -148,7 +148,7 @@ class TestDiff(unittest.TestCase):
     @weight(2)
     def test04(self):
         #Title used by Gradescope 
-        """HW3A - Invalid Input"""
+        """HW3A - Invalid Input & Exit"""
 
         # Create a subprocess to run the students code and with our test file
         cat = subprocess.Popen(["cat", "/autograder/source/input/inA4.txt"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -167,9 +167,9 @@ class TestDiff(unittest.TestCase):
         cat.kill()
         ref.kill()
 
-        print(f"Input Sequence: 55, 0\n")
+        print(f"Input Sequence: -85, 51, 0\n")
 
-        result = compare_strings(output, reference, 2)
+        result = compare_strings(output, reference, 3)
         print(result)
 
         # Standard unit test case with an associated error message
@@ -205,10 +205,149 @@ class TestDiff(unittest.TestCase):
             print("COMPILATION SUCCESSFUL!!")
         
         test.terminate()
+    
+    # Associated point value within GradeScope
+    @weight(0)
+    def test06(self):
+        #Title used by Gradescope 
+        """HW3B - Testcase - 1"""
+
+        # Create a subprocess to run the students code and with our test file
+        cat = subprocess.Popen(["cat", "/autograder/source/input/inB1.txt"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        # Use subprocess.run to execute the command with a timeout
+        try:
+            test = subprocess.run(["./HW3B"], stdin=cat.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, timeout=10)
+            output = test.stdout.strip()
+        except subprocess.TimeoutExpired:
+            cat.kill()
+            output = "Timeout expired!!"
+
+        print(f"Initial Vaue: 255\nNo. of Iterations: 10\nUpdate Size: ±10\n\nYour Output\n\n{output}")
+
+        num_lines = len(output.splitlines())
+
+        # Standard unit test case with an associated error message
+        if num_lines > 14:
+            print("\nNumber of iterations exceeded!!")
+            self.assertTrue(False, msg="")
+        if output != "Timeout expired!!":
+            print("\nProgram Exit Succesfully!!")
+            self.assertTrue(True, msg="")
+        else:
+            print("\nProgram did not Exit Successfully!!")
+            self.assertTrue(False, msg="")
+
+        cat.terminate()
+    
+    # Associated point value within GradeScope
+    @weight(0)
+    def test07(self):
+        #Title used by Gradescope 
+        """HW3B - Testcase - 2"""
+
+        # Create a subprocess to run the students code and with our test file
+        cat = subprocess.Popen(["cat", "/autograder/source/input/inB2.txt"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        # Use subprocess.run to execute the command with a timeout
+        try:
+            test = subprocess.run(["./HW3B"], stdin=cat.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, timeout=10)
+            output = test.stdout.strip()
+        except subprocess.TimeoutExpired:
+            cat.kill()
+            output = "Timeout expired!!"
+
+        print(f"Initial Vaue: 0\nNo. of Iterations: 20\nUpdate Size: ±2\n\nYour Output\n\n{output}")
+
+        num_lines = len(output.splitlines())
+
+        # Standard unit test case with an associated error message
+        if num_lines > 24:
+            print("\nNumber of iterations exceeded!!")
+            self.assertTrue(False, msg="")
+        if output != "Timeout expired!!":
+            print("\nProgram Exit Succesfully!!")
+            self.assertTrue(True, msg="")
+        else:
+            print("\nProgram did not Exit Successfully!!")
+            self.assertTrue(False, msg="")
+
+        cat.terminate()
+    
+    # Associated point value within GradeScope
+    @weight(2)
+    def test08(self):
+        #Title used by Gradescope 
+        """HW3B - Invalid Input"""
+
+        cat = subprocess.Popen(["cat", "/autograder/source/input/inB3.txt"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        try:
+            test = subprocess.run(["./HW3B"], stdin=cat.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, timeout=5)
+            output = test.stdout.strip()
+        except subprocess.TimeoutExpired:
+            cat.kill()
+            output = "Timeout expired!! Program did not exit."
+
+        reference = subprocess.check_output(["head", "-n", "4", "/autograder/source/reference/refB3.txt"]).decode('utf-8').strip()
+
+        cat.kill()
+
+        print(f"Input Sequence: 260, 256, -9\n")
+
+        result = compare_strings(output, reference, 4)
+        print(result)
+
+        # Standard unit test case with an associated error message
+        if reference in output:
+            print("\nPASSED!!")
+            self.assertTrue(True, msg="")
+        else:
+            print("\nFAILED!!")
+            self.assertTrue(False, msg="")
+        
+        cat.terminate()
+    
+    # Associated point value within GradeScope
+    @weight(2)
+    def test09(self):
+        #Title used by Gradescope 
+        """HW3B - I/O Formatting"""
+
+        # Create a subprocess to run the students code and with our test file
+        cat = subprocess.Popen(["cat", "/autograder/source/input/inB4.txt"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        
+        # Use subprocess.run to execute the command with a timeout
+        try:
+            test = subprocess.run(["./HW3B"], stdin=cat.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, timeout=5)
+            output = test.stdout.strip()
+        except subprocess.TimeoutExpired:
+            cat.kill()
+            output = "Timeout expired!! Program did not exit."
+
+        ref = subprocess.Popen(["cat", "/autograder/source/reference/refB4.txt"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        reference = ref.stdout.read().strip().decode('utf-8')
+
+        cat.kill()
+        ref.kill()
+
+        print(f"Initial Vaue: 200\nNo. of Iterations: 10\nUpdate Size: 0\n")
+
+        result = compare_strings(output, reference, 13)
+        print(result)
+
+        # Standard unit test case with an associated error message
+        if(self.assertTrue(output == reference, msg="")):
+            print("\nFAILED!!")
+        else:
+            print("\nPASSED!!")
+
+        cat.terminate()
+        ref.terminate()
 
     # Associated point value within GradeScope
     @weight(0)
-    def test15(self):
+    def test10(self):
         #Title used by Gradescope 
         """HW3C - Compilation Test"""
 
@@ -231,7 +370,7 @@ class TestDiff(unittest.TestCase):
     
     # Associated point value within GradeScope
     @weight(0)
-    def test16(self):
+    def test11(self):
         #Title used by Gradescope 
         """HW3C - Output"""
 
@@ -245,7 +384,6 @@ class TestDiff(unittest.TestCase):
         print(f"Your Output\n\n{output}")
 
         num_lines = len(output.splitlines())
-
 
         # Standard unit test case with an associated error message
         if num_lines > 29:
