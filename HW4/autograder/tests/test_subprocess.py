c9a29ef55b0408e97c85149c4b348c131cef39c6
Nishanth Somashekara Murthy <nishanthsmurthy24@gmail.com>
2024-02-20 12:34:06 -0600

    Added testcases for HW4C

--- HW4/autograder/tests/test_subprocess.py
+++ HW4/autograder/tests/test_subprocess.py
@@ -107,8 +107,11 @@ class TestDiff(unittest.TestCase):
             result = compare_strings(statistics_content, reference_content, 6)
             print(result)
 
+            print(statistics_content)
+            print(reference_content)
+
             # Standard unit test case with an associated error message
-            if statistics_content == reference_content:
+            if statistics_content in reference_content:
                 print("\nPASSED!! Contents of 'statistics.csv' are correct.")
                 self.assertTrue(True, msg="")
             else:
@@ -222,8 +225,8 @@ class TestDiff(unittest.TestCase):
 
             # Compare the contents
             # result = compare_strings(statistics_content, reference_content, 6)
-            print(f"Expected Output:\n{reference_content}")
-            print(f"\nYour Output:\n{statistics_content}")
+            print(f"Expected Output:\n\n{reference_content}")
+            print(f"\nYour Output:\n\n{statistics_content}")
 
             self.assertTrue(True, msg="")
             
@@ -234,7 +237,7 @@ class TestDiff(unittest.TestCase):
 
     # Associated point value within GradeScope
     @weight(0)
-    def test20(self):
+    def test08(self):
         #Title used by Gradescope 
         """HW4C - Compilation Test"""
 
@@ -252,5 +255,147 @@ class TestDiff(unittest.TestCase):
             self.assertTrue( output == "", msg=output)
         else:
             print("COMPILATION SUCCESSFUL!!")
+            print(f"\nInput File - input_files/blahblah.txt\n")
+            print("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquam ac metus eget maximus. Suspendisse eget tincidunt ligula. Fusce aliquet leo nec bibendum tempor.") 
+            print("Cras elit dolor, aliquet non lacus a, vestibulum eleifend urna. Integer ultrices ex ac vulputate tristique. Vestibulum felis libero, lacinia at purus ut, convallis eleifend mi.")
+            print("Vestibulum in euismod orci, a euismod augue. Integer quam velit, lobortis sit amet malesuada sit amet, dignissim eget odio. Proin eu eros eget magna eleifend pellentesque ac nec tortor.")
+            print("Nunc mattis turpis et leo cursus, sit amet dictum nisl pellentesque. Cras ultricies leo tellus, a volutpat ipsum maximus at. Pellentesque aliquet, lorem eu gravida tincidunt,")
+            print("magna tortor laoreet est, at aliquam magna nisl in nisl. Praesent eget pretium dolor. Aliquam euismod, enim nec cursus maximus, nunc massa molestie ipsum, sed posuere dolor leo ut neque.")
+            print("Vestibulum id tortor nec justo venenatis tristique id et est. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse eget mollis augue.")
+            print("Nunc sem arcu, feugiat id porttitor sed, fermentum ac nunc. Vivamus lacinia ante vitae lacus commodo gravida. Praesent eget eros quis felis efficitur faucibus a ut nulla. Sed non accumsan ipsum.")
+            print("Praesent condimentum dolor rhoncus massa hendrerit vulputate. Nunc euismod congue quam. In nisl erat, vulputate ac metus hendrerit, congue varius metus. Sed leo felis, eleifend eget pulvinar eget,")
+            print("ultrices malesuada lorem. Ut massa leo, ullamcorper quis velit vel, vestibulum blandit tortor. Sed consequat semper pulvinar. In massa enim, suscipit eu vestibulum quis, feugiat id ante.")
+            print("Pellentesque nec dictum nisi, ut pretium quam. Aenean ut vestibulum mi, nec dapibus metus. Quisque rhoncus ex nulla, vel bibendum turpis tristique vitae. Vestibulum vel dolor libero.")
+            print("Etiam mauris sem, dignissim a libero in, ultricies sagittis ex. Vestibulum quis est mi. Sed at augue at lacus tristique rutrum. Aenean eu magna vitae orci rhoncus tincidunt eget sit amet massa.")
+            print("Donec eleifend lobortis turpis commodo sodales. Vestibulum sed tellus vel justo cursus ultricies sed at arcu. Curabitur iaculis id nisl eget mollis. Donec et sem at metus commodo molestie.")
+            print("Morbi sed congue odio. Fusce tempus sem a nulla congue, non vestibulum mi ornare. Proin eu purus vitae velit laoreet lacinia non quis metus. Cras at nisl non risus varius bibendum nec eget nulla.")
+            print("Morbi congue ac nulla eu viverra. Sed placerat faucibus mauris a ultricies. Sed pretium lorem at ex dapibus, eget lacinia nisl pretium. Integer sodales vehicula arcu sit amet interdum.")
+            print("Cras accumsan in magna at commodo. Suspendisse pretium velit vitae pellentesque posuere. Aliquam erat volutpat. Sed elementum urna eget justo faucibus, vel egestas magna hendrerit.")
+            print("Nullam volutpat tristique erat, ac aliquam nunc gravida vitae. Nunc ut libero gravida, rhoncus sapien ornare, molestie massa. Proin id dignissim nulla. Suspendisse euismod posuere lacus convallis suscipit.")
+            print("Sed dapibus elit arcu, sit amet posuere ex tincidunt nec. Nunc vel purus massa. Nullam scelerisque quam sed lacus finibus cursus. Ut tincidunt turpis eleifend finibus lobortis. Mauris aliquet finibus bibendum.")
         
         test.terminate()
+
+    # Associated point value within GradeScope
+    @weight(2)
+    def test09(self):
+        # Title used by Gradescope 
+        """HW4A - Output File - 'election_results.txt' """
+
+        try:
+            # Run the program and capture the output
+            test = subprocess.run(["./HW4C"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, timeout=5)
+            output = test.stdout.strip()
+        except subprocess.TimeoutExpired:
+            output = "Timeout expired!! Program did not exit."
+
+        # Check if "election_results.txt" file is generated
+        file_path = "/autograder/source/output_files/election_results.txt"
+        file_generated = os.path.isfile(file_path)
+
+        missed_file_path = "/autograder/source/election_results.txt"
+        file_generated_wrong_path = os.path.isfile(missed_file_path)
+
+        # Standard unit test case with an associated error message
+        if file_generated:
+            print("PASSED!! Found output File 'output_files/election_results.txt'")
+            self.assertTrue(True, msg="")
+        elif missed_file_path:
+            print("FAILED!! Found output File 'election_results.txt' in the root directory. It needs to be in 'output_files' directory.")
+            self.assertTrue(False, msg="")
+        else:
+            print("FAILED!! Could not Find output File 'election_results.txt'")
+            self.assertTrue(False, msg="")
+    
+    # Associated point value within GradeScope
+    @weight(5)
+    def test10(self):
+        # Title used by Gradescope 
+        """HW4C - Contents of 'output_files/election_results.txt' """
+
+        try:
+            # Run the program and capture the output
+            test = subprocess.run(["./HW4C"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, timeout=5)
+            output = test.stdout.strip()
+        except subprocess.TimeoutExpired:
+            output = "Timeout expired!! Program did not exit."
+
+        # Check if "output_files/election_results.txt" file is generated
+        file_path = "/autograder/source/output_files/election_results.txt"
+        file_generated = os.path.isfile(file_path)
+
+        # Read the contents of output_files/election_results.txt and ref.csv
+        if file_generated:
+            result_content = read_file_contents(file_path)
+            reference_content = read_file_contents("/autograder/source/reference/ref_election_results.txt")
+
+            # Compare the contents
+            result = compare_strings(result_content, reference_content, 5)
+            print(result)
+
+            # Standard unit test case with an associated error message
+            if result_content == reference_content:
+                print("\nPASSED!! Contents of 'output_files/election_results.txt' are correct.")
+                self.assertTrue(True, msg="")
+            else:
+                print("\nFAILED!! Contents of 'output_files/election_results.txt' are incorrect.")
+                self.assertTrue(False, msg="")
+        else:
+            # Print a message if output_files/election_results.txt is not generated
+            print(f"\nFAILED!! Result file 'output_files/election_results.txt' could not be found.")
+            self.assertTrue(False, msg="")
+    
+    # Associated point value within GradeScope
+    @weight(1)
+    def test11(self):
+        # Title used by Gradescope 
+        """HW4C - Winner"""
+
+        try:
+            # Run the program and capture the output
+            test = subprocess.run(["./HW4C"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, timeout=5)
+            output = test.stdout.strip()
+        except subprocess.TimeoutExpired:
+            output = "Timeout expired!! Program did not exit."
+
+        reference = subprocess.check_output(["tail", "-n", "1", "/autograder/source/reference/ref_election_IO.txt"]).decode('utf-8').strip()
+
+        # Compare the contents
+        print(f"Expected Output:\n{reference}\n")
+        print(f"Your Output:\n{output.splitlines()[-1]}")
+
+        # Standard unit test case with an associated error message
+        if reference == output.splitlines()[-1]:
+            print("\nPASSED!!")
+            self.assertTrue(True, msg="")
+        else:
+            print("\nFAILED!!")
+            self.assertTrue(False, msg="")
+
+    # Associated point value within GradeScope
+    @weight(2)
+    def test12(self):
+        # Title used by Gradescope 
+        """HW4C - I/O Formatting """
+
+        try:
+            # Run the program and capture the output
+            test = subprocess.run(["./HW4C"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, timeout=5)
+            output = test.stdout.strip()
+        except subprocess.TimeoutExpired:
+            output = "Timeout expired!! Program did not exit."
+
+        reference = subprocess.check_output(["head", "-n", "6", "/autograder/source/reference/ref_election_IO.txt"]).decode('utf-8').strip()
+    
+        # Compare the contents
+        result = compare_strings(output, reference, 6)
+        print(result)
+
+        # Standard unit test case with an associated error message
+        if reference in output:
+            print("\nPASSED!!")
+            self.assertTrue(True, msg="")
+        else:
+            print("\nFAILED!!")
+            self.assertTrue(False, msg="")
+        
