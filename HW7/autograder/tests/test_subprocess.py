a9a16e15ba2bdfe49050e236a8be54e8b044e261
Nishanth Somashekara Murthy <nishanthsmurthy24@gmail.com>
2024-04-06 00:11:06 -0500

    Added testcases for HW7

--- HW7/autograder/tests/test_subprocess.py
+++ HW7/autograder/tests/test_subprocess.py
@@ -3,10 +3,14 @@ from gradescope_utils.autograder_utils.decorators import weight
 import subprocess
 from difflib import Differ
 
-def compare_strings(string1, string2):
+def compare_strings(string1, string2, n):
     differ = Differ()
-    diff = list(differ.compare(string1.splitlines(), string2.splitlines()))
+    lines1 = string1.splitlines()[:n]
+    lines2 = string2.splitlines()[:n]
+    diff = list(differ.compare(lines1, lines2))
+    # diff = list(differ.compare(string1.splitlines(), string2.splitlines()))
     return '\n'.join(diff)
+
 
 class TestDiff(unittest.TestCase):
     def setUp(self):
@@ -34,3 +38,131 @@ class TestDiff(unittest.TestCase):
             print("COMPILATION SUCCESSFUL!!")
         
         test.terminate()
+    
+    # Associated point value within GradeScope
+    @weight(1)
+    def test01(self):
+        # Title used by Gradescope 
+        """HW7 - Testcase 1"""
+
+        try:
+            # Run the program and capture the output
+            cat = subprocess.Popen(["cat", "/autograder/source/input/in1.txt"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            test = subprocess.run(["./HW7"], stdin=cat.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, timeout=5)
+            output = test.stdout.strip()
+        except subprocess.TimeoutExpired:
+            output = "Timeout expired!! Program did not exit."
+
+        ref = subprocess.Popen(["cat", "/autograder/source/reference/ref1.txt"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        reference = ref.stdout.read().strip().decode('utf-8')
+
+        cat.kill()
+        ref.kill()
+
+        result = compare_strings(output, reference, 5)
+        print(result)
+
+        # Standard unit test case with an associated error message
+        if(self.assertTrue(output == reference, msg="")):
+            print("\nFAILED!!")
+        else:
+            print("\nPASSED!!")
+
+        cat.terminate()
+        ref.terminate()
+
+    # Associated point value within GradeScope
+    @weight(1)
+    def test02(self):
+        # Title used by Gradescope 
+        """HW7 - Testcase 2"""
+
+        try:
+            # Run the program and capture the output
+            cat = subprocess.Popen(["cat", "/autograder/source/input/in2.txt"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            test = subprocess.run(["./HW7"], stdin=cat.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, timeout=5)
+            output = test.stdout.strip()
+        except subprocess.TimeoutExpired:
+            output = "Timeout expired!! Program did not exit."
+
+        ref = subprocess.Popen(["cat", "/autograder/source/reference/ref2.txt"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        reference = ref.stdout.read().strip().decode('utf-8')
+
+        cat.kill()
+        ref.kill()
+
+        result = compare_strings(output, reference, 5)
+        print(result)
+
+        # Standard unit test case with an associated error message
+        if(self.assertTrue(output == reference, msg="")):
+            print("\nFAILED!!")
+        else:
+            print("\nPASSED!!")
+
+        cat.terminate()
+        ref.terminate()
+
+    # Associated point value within GradeScope
+    @weight(1)
+    def test03(self):
+        # Title used by Gradescope 
+        """HW7 - Testcase 3"""
+
+        try:
+            # Run the program and capture the output
+            cat = subprocess.Popen(["cat", "/autograder/source/input/in3.txt"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            test = subprocess.run(["./HW7"], stdin=cat.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, timeout=5)
+            output = test.stdout.strip()
+        except subprocess.TimeoutExpired:
+            output = "Timeout expired!! Program did not exit."
+
+        ref = subprocess.Popen(["cat", "/autograder/source/reference/ref3.txt"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        reference = ref.stdout.read().strip().decode('utf-8')
+
+        cat.kill()
+        ref.kill()
+
+        result = compare_strings(output, reference, 5)
+        print(result)
+
+        # Standard unit test case with an associated error message
+        if(self.assertTrue(output == reference, msg="")):
+            print("\nFAILED!!")
+        else:
+            print("\nPASSED!!")
+
+        cat.terminate()
+        ref.terminate()
+
+    # Associated point value within GradeScope
+    @weight(1)
+    def test04(self):
+        # Title used by Gradescope 
+        """HW7 - Testcase 4"""
+
+        try:
+            # Run the program and capture the output
+            cat = subprocess.Popen(["cat", "/autograder/source/input/in4.txt"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            test = subprocess.run(["./HW7"], stdin=cat.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, timeout=5)
+            output = test.stdout.strip()
+        except subprocess.TimeoutExpired:
+            output = "Timeout expired!! Program did not exit."
+
+        ref = subprocess.Popen(["cat", "/autograder/source/reference/ref4.txt"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        reference = ref.stdout.read().strip().decode('utf-8')
+
+        cat.kill()
+        ref.kill()
+
+        result = compare_strings(output, reference, 5)
+        print(result)
+
+        # Standard unit test case with an associated error message
+        if(self.assertTrue(output == reference, msg="")):
+            print("\nFAILED!!")
+        else:
+            print("\nPASSED!!")
+
+        cat.terminate()
+        ref.terminate()
