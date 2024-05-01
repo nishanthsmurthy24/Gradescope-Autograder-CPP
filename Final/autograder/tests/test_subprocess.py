77a0e5828b004e9c969e60d27731060a1bc7801e
Nishanth Somashekara Murthy <nishanthsmurthy24@gmail.com>
2024-05-01 00:50:17 -0500

    Updated Visual interface for testcases

--- Final/autograder/tests/test_subprocess.py
+++ Final/autograder/tests/test_subprocess.py
@@ -55,7 +55,7 @@ class TestDiff(unittest.TestCase):
     @weight(0)
     def test01(self):
         #Title used by Gradescope 
-        """Testcase - 1"""
+        """Basic Testcase"""
 
         try:
             cat = subprocess.Popen(["cat", "/autograder/source/input/in1.txt"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -63,6 +63,10 @@ class TestDiff(unittest.TestCase):
             output = test.stdout.strip()
         except subprocess.TimeoutExpired:
             output = "Timeout expired!! Program did not exit."
+
+        print(f"<Number of Cars>\n\n<Model>\n<Tank Capacity> <MPG>\n<Refuel Quantity>\n<Miles to Drive>\n")
+
+        print(f"2\n\nSuburban\n44.0 12.0\n22\n24\n\nPrius\n10.0 50.0\n10\n200\n")
 
         reference = subprocess.check_output(["head", "-n", "2", "/autograder/source/reference/ref1.txt"]).decode('utf-8').strip()
 
@@ -85,7 +89,7 @@ class TestDiff(unittest.TestCase):
     @weight(0)
     def test02(self):
         #Title used by Gradescope 
-        """Testcase - 2"""
+        """Default Testcase"""
 
         try:
             cat = subprocess.Popen(["cat", "/autograder/source/input/in2.txt"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -94,7 +98,46 @@ class TestDiff(unittest.TestCase):
         except subprocess.TimeoutExpired:
             output = "Timeout expired!! Program did not exit."
 
-        reference = subprocess.check_output(["head", "-n", "5", "/autograder/source/reference/ref2.txt"]).decode('utf-8').strip()
+        print(f"<Number of Cars>\n\n<Make> <Model>\n<Tank Capacity> <MPG>\n<Refuel Quantity>\n<Miles to Drive>\n")
+    
+        print(f"2\n\nChevy Suburban\n44.0 12.0\n22\n24\n\nToyota Prius\n10.0 50.0\n10\n200\n")
+
+
+        reference = subprocess.check_output(["head", "-n", "2", "/autograder/source/reference/ref2.txt"]).decode('utf-8').strip()
+
+        cat.kill()
+
+        result = compare_strings(output, reference, 2)
+        print(result)
+
+        # Standard unit test case with an associated error message
+        if reference == output:
+            print("\nPASSED!!")
+            self.assertTrue(True, msg="")
+        else:
+            print("\nFAILED!!")
+            self.assertTrue(False, msg="")
+
+        cat.terminate()
+
+    # Associated point value within GradeScope
+    @weight(0)
+    def test03(self):
+        #Title used by Gradescope 
+        """Testcase - 1"""
+
+        try:
+            cat = subprocess.Popen(["cat", "/autograder/source/input/in3.txt"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            test = subprocess.run(["./final_exe"], stdin=cat.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, timeout=5)
+            output = test.stdout.strip()
+        except subprocess.TimeoutExpired:
+            output = "Timeout expired!! Program did not exit."
+        
+        print(f"<Number of Cars>\n\n<Make> <Model>\n<Tank Capacity> <MPG>\n<Refuel Quantity>\n<Miles to Drive>\n")
+
+        print(f"5\n\nChevy Camaro\n40.0 18.5\n2.5\n100\n\nToyota Corolla\n20.0 28.9\n10\n200\n\nDodge Charger\n20.0 23.5\n5\n10.5\n\nPorsche 911\n16.9 18\n5\n89.5\n\nAudi A6\n30.0 20.8\n32.0\n189.5\n")
+
+        reference = subprocess.check_output(["head", "-n", "5", "/autograder/source/reference/ref3.txt"]).decode('utf-8').strip()
 
         cat.kill()
 
@@ -113,18 +156,23 @@ class TestDiff(unittest.TestCase):
 
     # Associated point value within GradeScope
     @weight(0)
-    def test03(self):
+    def test04(self):
         #Title used by Gradescope 
-        """Testcase - 3"""
+        """Testcase - 2"""
 
         try:
-            cat = subprocess.Popen(["cat", "/autograder/source/input/in3.txt"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            cat = subprocess.Popen(["cat", "/autograder/source/input/in4.txt"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             test = subprocess.run(["./final_exe"], stdin=cat.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, timeout=5)
             output = test.stdout.strip()
         except subprocess.TimeoutExpired:
             output = "Timeout expired!! Program did not exit."
 
-        reference = subprocess.check_output(["head", "-n", "1", "/autograder/source/reference/ref3.txt"]).decode('utf-8').strip()
+        print(f"<Number of Cars>\n\n<Make> <Model>\n<Tank Capacity> <MPG>\n<Refuel Quantity>\n<Miles to Drive>\n")
+
+        print(f"0\n")
+
+
+        reference = subprocess.check_output(["head", "-n", "1", "/autograder/source/reference/ref4.txt"]).decode('utf-8').strip()
 
         cat.kill()
 
@@ -140,3 +188,4 @@ class TestDiff(unittest.TestCase):
             self.assertTrue(False, msg="")
 
         cat.terminate()
+    
